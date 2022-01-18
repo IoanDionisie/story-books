90cc64d93ffcd3f68aa944121c6baca6e9a9d77f
IoanDionisie <ioan.dionisie.ciuciu@gmail.com>
2022-01-18 17:54:55 +0200

    Added a bit routing

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const exphbs = require('express-handlebars');
 const morgan = require('morgan');
 
+//const { engine } = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
 // Load config
 dotenv.config({path: './config/cofig.env'});
@@ -15,6 +16,11 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+app.engine('hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', 'hbs');
+
+app.use('/', require('./routes/index'));
 
 const PORT = process.env.PORT || 5000;
 
